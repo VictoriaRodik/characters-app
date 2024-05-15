b914e6c4351b48562c53a3b068519a74697b7aad
VictoriaRodik <victoriarodik@gmail.com>
2024-05-15 13:14:12 +0300

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./characters-app",
+	base: "/characters-app/",
 	plugins: [react()],
 	test: {
 		environment: 'jsdom',
